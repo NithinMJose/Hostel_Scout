3e35205d4f90d057e4d84d8576c2ae16f740dc5d
nithinjose1 <nithin.jose@ayatacommerce.com>
2025-05-27 07:52:19 +0530

    Spring boot initialised: Spring boot project initialised

---
+++ hostelscout-springboot/src/main/java/com/hostelcout/HostelscoutApplication.java
@@ -0,0 +1,13 @@
+package com.hostelcout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelscoutApplication.class, args);
+	}
+
+}
---
+++ hostelscout-springboot/src/test/java/com/hostelcout/HostelscoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelcout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelscoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
